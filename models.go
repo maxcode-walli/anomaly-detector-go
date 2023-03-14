a0cb3132ca3990ca211b8ad40b01e4c98c93b871
Alexandra Savescu <alexandrasavescu92@gmail.com>
2023-03-14 13:47:06 +0200

    Fix models

--- models.go
+++ models.go
@@ -1,20 +1,20 @@
-package p
+package main
 
 type Transaction struct {
 	UserID                            string       `json:"userID,omitempty"`
 	ExternalAccountID                 string       `json:"externalAccountID,omitempty"`
-	TransactionStatus                 string       `json:"transactionStatus,omitempty"`
-	TransactionId                     string       `json:"transactionId,omitempty"`
-	Uuid                              string       `json:"uuid,omitempty"`
-	EndToEndId                        string       `json:"endToEndId,omitempty"`
-	TransactionAmount                 *Money       `json:"transactionAmount,omitempty"`
-	BookingDate                       string       `json:"bookingDate,omitempty"`
-	ValueDate                         string       `json:"valueDate,omitempty"`
-	BalanceAfterTransaction           string       `json:"balanceAfterTransaction,omitempty"`
-	RemittanceInformationUnstructured string       `json:"remittanceInformationUnstructured,omitempty"`
-	RemittanceInformationStructured   string       `json:"remittanceInformationStructured,omitempty"`
-	CreditorAccount                   *CounterPart `json:"creditorAccount,omitempty"`
-	DebtorAccount                     *CounterPart `json:"debtorAccount,omitempty"`
+	TransactionStatus                 string       `json:"transactionStatus,omitempty" firestore:"transactionStatus,omitempty"`
+	TransactionId                     string       `json:"transactionId,omitempty" firestore:"transactionId,omitempty"`
+	Uuid                              string       `json:"uuid,omitempty" firestore:"uuid,omitempty"`
+	EndToEndId                        string       `json:"endToEndId,omitempty" firestore:"endToEndId,omitempty"`
+	TransactionAmount                 *Money       `json:"transactionAmount,omitempty" firestore:"transactionAmount,omitempty"`
+	BookingDate                       string       `json:"bookingDate,omitempty" firestore:"bookingDate,omitempty"`
+	ValueDate                         string       `json:"valueDate,omitempty" firestore:"valueDate,omitempty"`
+	BalanceAfterTransaction           string       `json:"balanceAfterTransaction,omitempty" firestore:"balanceAfterTransaction,omitempty"`
+	RemittanceInformationUnstructured string       `json:"remittanceInformationUnstructured,omitempty" firestore:"remittanceInformationUnstructured,omitempty"`
+	RemittanceInformationStructured   string       `json:"remittanceInformationStructured,omitempty" firestore:"remittanceInformationStructured,omitempty"`
+	CreditorAccount                   *CounterPart `json:"creditorAccount,omitempty" firestore:"creditorAccount,omitempty"`
+	DebtorAccount                     *CounterPart `json:"debtorAccount,omitempty" firestore:"debtorAccount,omitempty"`
 }
 
 type Money struct {
@@ -28,11 +28,11 @@ type CounterPart struct {
 	MetaInfo map[string]interface{} `json:"metaInfo,omitempty"`
 }
 
-type User struct {
-	Email         string `json:"email,omitempty"`
-	DisplayName   string `json:"displayName,omitempty"`
-	PhotoUrl      string `json:"photoUrl,omitempty"`
-	Uid           string `json:"uid,omitempty"`
-	EmailVerified string `json:"emailVerified,omitempty"`
-	Job           string `json:"job,omitempty"`
+type AccountUser struct {
+	Email         string `firestore:"email,omitempty"`
+	DisplayName   string `firestore:"displayName,omitempty"`
+	PhotoUrl      string `firestore:"photoURL,omitempty"`
+	Uid           string `firestore:"uid,omitempty"`
+	EmailVerified bool   `firestore:"emailVerified"`
+	Job           string `firestore:"job"`
 }
